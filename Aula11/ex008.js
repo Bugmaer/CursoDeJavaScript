c453de70f3953b2d3df96b1ed3c9b9a305a49b19
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2024-08-25 12:30:44 -0300

    .

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log("A sua velocidade é maior que a permitida. MULTADO!!")
+}
+console.log('Dirija com cinto!!')
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'BRASIL'
+if (país == 'BRASIL') {
+    console.log("Você é brasileiro")
+}
+else {
+    console.log('Você é gringo')
+}
