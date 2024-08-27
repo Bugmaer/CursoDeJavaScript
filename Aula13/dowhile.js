3d5700932cff65f2cb09f2b7d3b79081aca99fa9
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2024-08-26 23:12:05 -0300

    .

--- Aula13/dowhile.js
+++ Aula13/dowhile.js
@@ -1,5 +1,6 @@
-var c = 1
-while (c < 6) {
+var c = 0
+do {
     console.log('tudo bem')
     c++
-}
+}  while (c < 6)
+
---
+++ Exercicios/aula14/ex03.html/ex04.html/script.js
@@ -0,0 +1,5 @@
+function verificar() {
+    var ano = Number(document.getElementsByName('ano'))
+    agora = new Date
+    ano = get
+}
