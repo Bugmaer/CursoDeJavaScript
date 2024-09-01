ccc10a05c6623f3e6ebf167267fb2fbe273fa2b2
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2024-09-01 17:32:31 -0300

    aula 18 ainda

--- Aula18/modulo.js
+++ Aula18/modulo.js
@@ -1,9 +1,12 @@
 import getTodosCursos from "./cursos.js"
-import { cursos,getCruso } from "./cursos.js"
+//import { cursos as c,getCruso as gc } from "./cursos.js"
+import * as m_cursos from './cursos.js'
 
 
+console.log(m_cursos.getCruso(1))
+console.log(m_cursos.cursos)
+console.log(m_cursos.default())
+//console.log(getTodosCursos())
 
-console.log('Rodando')
-console.log(cursos)
-console.log(getCruso(1))
+
 
