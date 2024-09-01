7f2c50c3c6c695affbcda4ef65fecc60003cc714
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2024-09-01 17:17:20 -0300

    aula 18

---
+++ Aula18/cursos.js
@@ -0,0 +1,5 @@
+const cursos = ['JavaScript', 'HTML', 'CSS', 'Arduino', 'CSHARP', 'JAVA', 'C++']
+const carros = ['POLO', 'T-CROSS', 'GOLF', 'ONIX', "CRONOS", "ARGO"]
+
+export {cursos}
+export {carros}
